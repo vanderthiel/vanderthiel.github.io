e5df9bc455203eeb09cee87ec56e8efe6fac40b9
Thiel, Edwin van der <edwin.van.der.thiel@sogeti.com>
2018-10-18 19:12:51 +0200

    it was late...

--- bluetooth.js
+++ bluetooth.js
@@ -1,4 +1,5 @@
 button.addEventListener('pointerup', function(event) {
+  console.log("zoek de blauwe tand");
   navigator.bluetooth.requestDevice({
     acceptAllDevices: true,
     optionalServices: ['battery_service']
